f64813fa8db945ca59e0970e3aadeecca0e4427f
Mohammed Hanaf <hanaf.n.m@gmail.com>
2020-02-09 17:38:22 +0530

    Added Readme, code cleanup

--- src/main/java/com/de/gpi/indexapi/IndexApiApplication.java
+++ src/main/java/com/de/gpi/indexapi/IndexApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IndexapiApplication {
+public class IndexApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IndexapiApplication.class, args);
+		SpringApplication.run(IndexApiApplication.class, args);
 	}
 
 }
